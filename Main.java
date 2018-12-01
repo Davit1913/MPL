26440069f353e2d7ef085ffb31e00c516a038900
Davit Karapetyan <davit1913@gmail.com>
2018-12-01 22:54:15 +0400

    Main is ready

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		
+		Point p1 = new Point(1, 2);
+		Point p2 = new Point(3. 4);
+
+		Line line = new Line(p1, p2);
+
+		System.out.println(p1 + "\n" + p2 + "\n" + line);
 	}
 }
